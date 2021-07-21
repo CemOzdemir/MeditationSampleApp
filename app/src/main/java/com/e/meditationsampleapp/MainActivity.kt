3cdcae07bc0a5963c6356b7e7ae163ca4fa23975
Cem Ozdemir <cemozdemir@mail.com>
2021-07-21 14:14:23 +0300

    [cemozdemir@mail.com][Start of the project.]

---
+++ app/src/main/java/com/e/meditationsampleapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.e.meditationsampleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.e.meditationsampleapp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
